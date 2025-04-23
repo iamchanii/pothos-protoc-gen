0c25ca8d8aac71515e51c6f87e7d829f118d187e
Chanhee Lee <26643843+iamchanii@users.noreply.github.com>
2025-04-23 10:50:41 +0900

    Fix collect services in case about same method names exists (#2)

--- src/collect/collect-all-methods.ts
+++ src/collect/collect-all-methods.ts
@@ -4,7 +4,7 @@ import type { DescMethod, DescService } from '@bufbuild/protobuf';
  * Collects all non-deprecated methods from a set of services into a collection map.
  *
  * @param services - An iterable of service descriptors to collect methods from
- * @param collection - A map to store the collected methods, with method names as keys
+ * @param collection - A map to store the collected methods, with service name and method name as keys
  *
  * This function iterates through all provided services and their methods, adding each non-deprecated
  * method to the collection map if a method with the same name doesn't already exist in the collection.
@@ -18,8 +18,9 @@ export function collectAllMethods(
     for (const method of service.methods) {
       if (method.deprecated || method.input.deprecated) continue;
 
-      if (!collection.has(method.name)) {
-        collection.set(method.name, method);
+      const collectionKey = `${method.parent.typeName}.${method.name}`;
+      if (!collection.has(collectionKey)) {
+        collection.set(collectionKey, method);
       }
     }
   }
